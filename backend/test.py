e3985a7c454730d9e796945cdb78a04095d238f2
12436894 <safeerehtesham@gmail.com>
2026-05-13 21:42:54 +0500

    chore: remove unused file configurations and associated references

--- backend/test.py
+++
@@ -1,6 +0,0 @@
-from app.services.agent import graph
-
-config = {"configurable": {"thread_id": "thread_123"}}
-state = graph.get_state(config)
-for message in state.values["messages"]:
-    print(f"{message.type}: {message.content}")
